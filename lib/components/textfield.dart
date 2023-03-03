ec67c0e510e20ed6bd467885f3fd9a4634fa4b22
Oussama Souayri <souayri.oussama@gmail.com>
2023-03-03 12:19:47 +0100

    add text field and button components

--- lib/components/textfield.dart
+++ lib/components/textfield.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 class Textfield extends StatelessWidget{
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const Textfield({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
   @override
   Widget build(BuildContext context) {
-      return const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 25.0),
+      return  Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
         child: TextField(
+          controller: controller,
+          obscureText: obscureText,
           decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Color(0xFFF1F3F6)
-                ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Color(0xFFF1F3F6)),
               ),
-              focusedBorder: OutlineInputBorder(
+              focusedBorder: const OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.black)
               ),
               fillColor: Color(0xFFF1F3F6),
-              filled: true
+              filled: true,
+              hintText: hintText,
+              hintStyle: TextStyle(color: Colors.grey[500])
           ),
         ),
       );
